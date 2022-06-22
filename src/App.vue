345295464eee5e0204bb5545012f41bc6f843ed3
Alberto Gualis <alberto.gualis@rebilly.com>
2022-06-22 12:15:06 +0200

    Add named slots example

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
   <r-button @click="onClick">Click me!</r-button>
+
+   <v-form-tile>
+        <template #title>
+            <h2>Profile Summary</h2>
+        </template>
+
+        <r-button #description>Button with slot </r-button>
+
+        <template #content>
+          <r-button>Button inside slot with template</r-button>
+        </template>
+    </v-form-tile>
+
 <!-- v-model {{numValue}}
 <r-input v-model="numValue" @change="change">Type a value</r-input> -->
 <!-- <br>
@@ -15,6 +28,7 @@
 </template>
 
 <script setup>
+    import  VFormTile from './v-form-tile.vue'
     import {ref} from '@vue/reactivity';
     const value = ref({start: new Date('01-01-2022'), end: new Date('01-02-2022')});
     const numValue = ref(24);
---
+++ src/v-form-tile.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="grid">
+        <div class="grid-item grid-item-secondary">
+            <div class="tile tile-description">
+                <div class="tile-title">
+                    <slot name="title"></slot>
+                </div>
+                <div class="tile-content">
+                    <slot name="description"></slot>
+                </div>
+            </div>
+        </div>
+        <div class="grid-item grid-item-primary">
+            <slot name="content"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'v-form-tile',
+    };
+</script>
